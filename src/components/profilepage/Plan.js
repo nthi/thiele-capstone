33706e67fa54726778f925b7b927574a1ac642a9
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-08-23 15:02:54 -0400

    Begin adding form to PUT changes to activity plans to database.

--- src/components/profilepage/Plan.js
+++ src/components/profilepage/Plan.js
@@ -3,8 +3,33 @@
 //TO DO: I'll need a DELETE button if logged plan
 //a DELETE button for saved plans
 //an add-notes form/save-and-log button for saved plans to PUT changes to the bridge table in "note" key
+
+import { useState } from "react"
+
 //when form is edited and/or saved-and-logged, page display should refresh immediately and the saved plan which was logged will now appear with notes and a timestamp in the logged display.
 export const Plan = ({plan, activities}) => {
+
+    const [planNote, updatePlanNote] = useState({
+        date: "",
+        note: "",
+        isLogged: ""
+    })
+
+    const updatePlan = () => {
+        const copy = {
+            date: new Date(),
+            note: planNote.note,
+            isLogged: true
+        }
+        fetch(`http://localhost:8088/userActivityBridge?isLogged=false&userId=${wswdObject.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(copy)
+        })
+            .then(response => response.json())
+    }
 
 
     let actOne = activities.find(x=> x.id === plan.activityOneId)
@@ -26,10 +51,36 @@ export const Plan = ({plan, activities}) => {
             <div>
             {
                 plan.date
-                ? `Date: ${plan.date} `
+                ? `Date: ${plan.date}`
                 : ""
             }
             </div>
+
+            <div>
+                {
+                    !plan.isLogged
+                    ? <fieldset>
+                    <div className="form-group">
+                    <label htmlFor="contents"></label>
+                        <input
+                            required autoFocus
+                            type="text"
+                            className="form-control"
+                            placeholder="Add notes..."
+                            value={planNote.note}
+                            onChange={
+                                (evt) => {
+                                    const copy = { ...planNote }
+                                    copy.note = evt.target.value
+                                    updatePlanNote(copy)
+                                }
+                            } />
+                    </div>
+                    </fieldset>
+                : ""
+                }
+            </div>
+            
         </div>
     )
 }
