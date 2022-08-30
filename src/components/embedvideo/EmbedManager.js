df192555500e18fca7bc4a092097167ae7851ce4
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-08-30 16:05:30 -0400

    Add more algo logic to EmbedManager.js

--- src/components/embedvideo/EmbedManager.js
+++ src/components/embedvideo/EmbedManager.js
@@ -2,12 +2,15 @@
 //give my video div an id and link set to id can let me jump
 //put something in state to help me know what vid, then in part at bottom retrieve that state...
 
-//I will need a slice/split function
+//I will need a split function
 //a clickHandler function
 //a component that manages the results
 
-//this will be child of MainPage.js, take props
+//this will be child of MainPage.js, possibly grandchild of DisplayPlan.js, take props
 
 export const EmbedManager = () => {
-    let embedId
+    //get YouTube links from state on MainPage/DisplayPlan?
+    //if (plan.link.includes("youtube") )
+    //let embedIdArr =  plan.link.split("?v=")
+    //that will give me [first half of youtube addy, the thing I want for src embedID in YoutubeEmbed.js]
 }
