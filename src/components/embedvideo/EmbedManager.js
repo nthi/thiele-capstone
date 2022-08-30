cb935caad3c1c0861f04ae53cacca6edeea77af3
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-08-30 15:23:28 -0400

    Algorithm notes in MainPage.js. Creates EmbedMananager.js

---
+++ src/components/embedvideo/EmbedManager.js
@@ -0,0 +1,13 @@
+//slice or split the links to get just that last bit
+//give my video div an id and link set to id can let me jump
+//put something in state to help me know what vid, then in part at bottom retrieve that state...
+
+//I will need a slice/split function
+//a clickHandler function
+//a component that manages the results
+
+//this will be child of MainPage.js, take props
+
+export const EmbedManager = () => {
+    let embedId
+}
--- src/components/mainpage/MainPage.js
+++ src/components/mainpage/MainPage.js
@@ -1,6 +1,7 @@
 //This module will render main page display components (inside classed divs for styling purposes).
 //TO DO: for stretch goals, add toggle-button elements to opt-in/out of certain categories
 //TO DO: for stretch goals, add custom-submission-form where user can add a link that will automatically appear in their plan or add it to the activity table to be included in the future.
+//this will be parent of EmbedManager.js, needs to set state and create props
 
 import { DisplayPlan } from "./DisplayPlan"
 import { RandomPlan } from "./RandomPlan"
@@ -27,18 +28,11 @@ export const MainPage = () => {
         </button>
         </div>
 
-        <div className="embedYT">
+        {/* <div className="embedYT">
             <h4>Watch YouTube Links Here:</h4>
             <YoutubeEmbed embedId="KV8Hj_E8LJc"/>
-        </div>
+        </div> */}
     </>
     )
 }
 
-//slice or split the links to get just that last bit
-//give my video div an id and link set to id can let me jump
-//put something in state to help me know what vid, then in part at bottom retrieve that state...
-
-//I will need a slice/split function
-//a clickHandler function
-//a component that manages the results
